c8950e9635ca2d928175186e58a2e025ae0d1e49
Marny Lopez - AKC MAC <marny@local.akc.org>
2020-04-20 22:58:24 -0600

    added bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import DeckList from "./components/DeckList";
+import AddDeck from "./components/AddDeck";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "AddDeck") {
+              iconName = focused
+                ? "ios-add-circle"
+                : "ios-add-circle-outline";
+            } else if (route.name === "Decks") {
+              iconName = focused ? "ios-list-box" : "ios-list";
+            }            
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Decks" component={DeckList} />
+        <Tab.Screen name="AddDeck" component={AddDeck} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
